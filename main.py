9bcedf6d2a4d1d9f1413ade3b8ae9f7c8901b8b0
ts549 <sun955@purdue.edu>
2022-05-15 15:07:24 -0400

    Scrape review titles

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from parser.Parser import Parser
 
-print(Parser.getHTML('https://www.amazon.com'))
+url = 'https://www.amazon.com/Amazon-Essentials-Straight-Fit-Jogger-Khaki/product-reviews/B07F2K9R2T/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews'
+
+parser = Parser(url)
+
+first = True
+
+while (first or next_ref != None):
+    first = False
+    page = parser.get_page()
+
+    if (page == None): break
+
+    reviews = parser.get_reviews(page)
+
+    for review in reviews:
+        print(review.find('a', {'data-hook': 'review-title'}).text)
+
+    next_ref = parser.get_next_page(page)
+
+    if next_ref != None:
+        next_path = next_ref.contents[0].get('href')
+        parser.set_path(next_path)
--- parser/Parser.py
+++ parser/Parser.py
@@ -1,12 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 import pickle
 
-class Parser(object):
+class Parser():
 
-    @staticmethod
-    def getHTML(url):
+    def __init__(self, url):
+        sects = urlparse(url)
+        self.scheme = sects[0]
+        self.netloc = sects[1]
+        self.path = sects[2]
+        self.hostname = self.scheme + '://' + self.netloc
+
+    def get_page(self):
+        url = self.hostname + self.path
         params = {'url': url, 'wait': 2}
         page = requests.get('http://localhost:8050/render.html', params=params)
 
-        return page.text
+        return page
+
+    def get_reviews(self, page):
+        soup = BeautifulSoup(page.text, 'html.parser')
+        reviews = soup.find_all('div', {'data-hook': 'review'})
+        return reviews
+
+    def get_next_page(self, page):
+        soup = BeautifulSoup(page.text, 'html.parser')
+        ref = soup.find('li', {'class': 'a-last'})
+        return ref
+
+    def set_path(self, path):
+        self.path = path
+
+    def print_details(self):
+        print(self.scheme)
+        print(self.netloc)
+        print(self.hostname)
+        print(self.path)
